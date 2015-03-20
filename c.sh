9c8b0d3ad1a6dd8994ce4505c4136a593260fd4f
Walter Perez <wperezurcia@gmail.com>
2015-03-19 22:51:59 -0300

    script complete for c)

--- c.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Eliminando archivos antiguos de entrada"
-rm data/max2sat_100at/in/*
-echo "Eliminando archivos antiguos de salida"
-rm data/max2sat_100at/out/*
-echo "Generando datos de entrada"
-sh c_in.sh
-echo "Ejecutando algoritmo y guardando datos de salida"
-sh c_out.sh
